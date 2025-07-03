8989b8b8ea92eda6cf35430883585b84d1858f38
HJ1263532 <osisland2918@gmail.com>
2025-07-03 17:59:39 +0900

    세션에 정보표시

--- backend/src/main/java/SANTA/backend/controller/MainController.java
+++ backend/src/main/java/SANTA/backend/controller/MainController.java
@@ -1,8 +1,14 @@
 package SANTA.backend.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,6 +17,16 @@ public class MainController {
     @GetMapping("/")
     public String mainP(){
 
-        return "main Controller";
+        //username 가져오기
+        String username=  SecurityContextHolder.getContext().getAuthentication().getName();
+        //role값 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "main Controller\n"+username+role;
     }
 }
