2c59000e48415f4dc7d84533fbbf831bd7136135
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-08 16:17:36 +0900

    feat: userGroup 엔티티와 일대다 매핑 & refactor: lombok을 이용한 생성자 구현으로 변경

--- backend/src/main/java/SANTA/backend/core/user/entity/UserEntity.java
+++ backend/src/main/java/SANTA/backend/core/user/entity/UserEntity.java
@@ -1,17 +1,23 @@
 package SANTA.backend.core.user.entity;
 
+import SANTA.backend.core.group.entity.UserGroupEntity;
 import SANTA.backend.core.user.domain.Role;
-import SANTA.backend.core.user.domain.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter//Entity는 setter설정XXX
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long userId; //->이거 string형임
 
     private String username;
@@ -20,17 +26,7 @@ public class UserEntity {
     private int age;
     private Role role;
 
-    protected UserEntity(){
-
-    }
-
-    public UserEntity(User user){
-        this.userId = user.getUserId();
-        this.username = user.getUsername();
-        this.password = user.getPassword();
-        this.nickname = user.getNickname();
-        this.age = user.getAge();
-        this.role = user.getRole();
-    }
+    @OneToMany(mappedBy = "USER_ID")
+    private List<UserGroupEntity> userGroups = new ArrayList<>();
 
 }
