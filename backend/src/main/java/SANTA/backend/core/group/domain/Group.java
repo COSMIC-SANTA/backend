4d3b15f2158cc94bc88df72474262e1912d3d253
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-08 16:18:03 +0900

    feat: group 도메인 구현

--- backend/src/main/java/SANTA/backend/core/group/domain/Group.java
+++ backend/src/main/java/SANTA/backend/core/group/domain/Group.java
@@ -1,2 +1,40 @@
-package SANTA.backend.core.group.domain;public class Group {
+package SANTA.backend.core.group.domain;
+
+import SANTA.backend.core.group.entity.GroupEntity;
+import lombok.Builder;
+
+public class Group {
+
+    Long id;
+
+    String location;
+
+    String title;
+
+    Integer age;
+
+    Interest interest;
+
+    Level level;
+
+    @Builder
+    protected Group(Long id, String location, String title, Integer age, Interest interest, Level level) {
+        this.id = id;
+        this.location = location;
+        this.title = title;
+        this.age = age;
+        this.interest = interest;
+        this.level = level;
+    }
+
+    public Group fromEntity(GroupEntity groupEntity) {
+        return Group.builder()
+                .id(groupEntity.getId())
+                .location(groupEntity.getLocation())
+                .title(groupEntity.getTitle())
+                .age(groupEntity.getAge())
+                .interest(groupEntity.getInterest())
+                .level(groupEntity.getLevel())
+                .build();
+    }
 }
