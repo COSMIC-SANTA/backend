988c65ceae7090240cc4ed93952ccea4cd10251e
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-13 14:49:56 +0900

    feat: 한국 관광공사 api 호출 구현체 구현

--- backend/src/main/java/SANTA/backend/global/utils/api/KoreanTourInfoServiceRequester.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/KoreanTourInfoServiceRequester.java
@@ -1,2 +1,23 @@
-package SANTA.backend.global.utils.api;public class KoreanTourInfoServiceRequester {
+package SANTA.backend.global.utils.api;
+
+import SANTA.backend.global.common.AppProperties;
+import SANTA.backend.global.config.RabbitMQConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KoreanTourInfoServiceRequester implements APIRequestProducer{
+
+    private final RabbitTemplate rabbitTemplate;
+    private final AppProperties appProperties;
+
+    @Override
+    public void sendRequest() {
+        String exchange = RabbitMQConfig.INFO_SERVICE_DLX;
+        String routingKey = appProperties.getKoreaTourOrganization().getKoreaTourInfoServiceRoutingKey();
+        String url = appProperties.getKoreaTourOrganization().getKoreaTourInfoServiceURL();
+        rabbitTemplate.convertAndSend(exchange,routingKey,url);
+    }
 }
