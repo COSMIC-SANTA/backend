fb3530c742a81a80cc7171f0da89d9428fdc3e67
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-13 17:03:40 +0900

    feat: 배너용 api 구현

--- backend/src/main/java/SANTA/backend/core/mountain/api/BannerApi.java
+++ backend/src/main/java/SANTA/backend/core/mountain/api/BannerApi.java
@@ -1,6 +1,7 @@
 package SANTA.backend.core.mountain.api;
 
 import SANTA.backend.core.auth.service.CustomUserDetails;
+import SANTA.backend.core.mountain.application.BannerService;
 import SANTA.backend.core.mountain.dto.BannerResponse;
 import SANTA.backend.core.user.domain.Interest;
 import lombok.RequiredArgsConstructor;
@@ -13,14 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/main")
-public class MainApi{
+public class BannerApi {
 
-    private final 
+    private final BannerService bannerService;
 
     @GetMapping
     public ResponseEntity<BannerResponse> getBanner(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         Interest interest = customUserDetails.getInterest();
-
     }
 
 }
