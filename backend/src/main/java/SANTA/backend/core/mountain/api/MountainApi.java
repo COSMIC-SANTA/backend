d878167aaff71ce234861c6974453a979f59f22b
Sin Sohi <sohi8451@naver.com>
2025-07-13 17:39:14 +0900

    feat: 산 검색을 위한 API 구현

---
+++ backend/src/main/java/SANTA/backend/core/mountain/api/MountainApi.java
@@ -0,0 +1,25 @@
+package SANTA.backend.core.mountain.api;
+
+import SANTA.backend.core.mountain.application.MountainService;
+import SANTA.backend.core.mountain.dto.MountainListSearchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mountains")
+@RequiredArgsConstructor
+public class MountainApi {
+
+    private final MountainService mountainService;
+
+    @GetMapping("search/{mountainName}")
+    public ResponseEntity<MountainListSearchResponse> searchMountain(@PathVariable String mountainName){
+
+        MountainListSearchResponse response = mountainService.searchMountains(mountainName.trim());
+        return ResponseEntity.ok(response);
+    }
+}
