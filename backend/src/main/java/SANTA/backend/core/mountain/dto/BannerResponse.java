dd9762a5b7f90a2364d9bf9c19008437a46e68fe
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 09:45:44 +0900

    feat: 배너 표시용 DTO 생성

--- backend/src/main/java/SANTA/backend/core/mountain/dto/BannerResponse.java
+++ backend/src/main/java/SANTA/backend/core/mountain/dto/BannerResponse.java
@@ -4,8 +4,14 @@ import SANTA.backend.core.mountain.domain.Mountain;
 import SANTA.backend.core.user.domain.Interest;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-public record BannerResponse() {
-    static Interest interest;
-    static List<Mountain> mountains;
+public record BannerResponse(
+        Interest interest,
+        List<BannerMountainResponse> mountains
+) {
+
+    public static BannerResponse from(Interest interest, List<Mountain> mountains){
+        return new BannerResponse(interest,mountains.stream().map(BannerMountainResponse::from).toList());
+    }
 }
