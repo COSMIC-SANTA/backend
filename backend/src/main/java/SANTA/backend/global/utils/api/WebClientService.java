0f0a22a2dbc141a9359df6a0d4d4f05f38e23b61
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 17:36:09 +0900

    feat: WebClient를 이용한 외부 API 호출용 서비스 객체 구현

--- backend/src/main/java/SANTA/backend/global/utils/api/WebClientService.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/WebClientService.java
@@ -1,2 +1,45 @@
-package SANTA.backend.global.utils.api;public class WebClientService {
+package SANTA.backend.global.utils.api;
+
+import SANTA.backend.global.utils.api.domain.TouristApiResponse;
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class WebClientService {
+
+    private final WebClient webClient;
+    private static final String JSON = "json";
+
+    public TouristApiResponse request(String url, String operation, String key, String mobileOs, String mobileApp) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(url+"/"+operation)
+                        .queryParam("serviceKey", key)
+                        .queryParam("MobileOS", mobileOs)
+                        .queryParam("MobileApp", mobileApp)
+                        .queryParam("_type",JSON)
+                        .build())
+                .retrieve().bodyToMono(TouristApiResponse.class)
+                .block();
+    }
+
+    public TouristApiResponse request(String url, String operation, String key, String mobileOs, String mobileApp, int contentTyeId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(url+"/"+operation)
+                        .queryParam("serviceKey", key)
+                        .queryParam("MobileOS", mobileOs)
+                        .queryParam("MobileApp", mobileApp)
+                        .queryParam("contentTypeId",contentTyeId)
+                        .queryParam("_type",JSON)
+                        .build())
+                .retrieve().bodyToMono(TouristApiResponse.class)
+                .block();
+    }
+
+
+
 }
