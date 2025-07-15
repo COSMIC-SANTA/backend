0f3bd95379bfd2f028102cfaa14019f997b97860
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 10:01:15 +0900

    feat: 관심사로 산 찾는 메서드 구현

--- backend/src/main/java/SANTA/backend/core/mountain/infra/MountainJPARepository.java
+++ backend/src/main/java/SANTA/backend/core/mountain/infra/MountainJPARepository.java
@@ -1,2 +1,25 @@
-package SANTA.backend.core.mountain.infra;public class MountainJPARepository {
+package SANTA.backend.core.mountain.infra;
+
+import SANTA.backend.core.mountain.domain.Mountain;
+import SANTA.backend.core.mountain.domain.MountainRepository;
+import SANTA.backend.core.user.domain.Interest;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MountainJPARepository implements MountainRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public List<Mountain> findByInterest(Interest interest){
+        return em.createQuery("select m from MountainEntity m where m.interest =: interest", Mountain.class)
+                .setParameter("interest",interest)
+                .getResultList();
+    }
+
 }
