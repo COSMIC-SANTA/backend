90a842b67d9b4a5dc93cb0caf7d3f550eece02db
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 12:02:57 +0900

    feat: 산 저장 로직 테스트

--- backend/src/test/java/SANTA/backend/mountain/infra/MountainRepositoryTest.java
+++ backend/src/test/java/SANTA/backend/mountain/infra/MountainRepositoryTest.java
@@ -6,6 +6,7 @@ import SANTA.backend.core.mountain.domain.Mountain;
 import SANTA.backend.core.mountain.dto.BannerResponse;
 import SANTA.backend.core.user.domain.Interest;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -72,5 +73,54 @@ public class MountainRepositoryTest extends RepositoryContext {
             );
         }
 
+    }
+
+    @Nested
+    class 저장용_테스트{
+
+        @Test @Transactional
+        void 산을_저장한다(){
+            //given
+            Long mountainCount = 3L;
+
+            Mountain mountain1 = Mountain.builder()
+                    .visitCount(10L)
+                    .sequence(0L)
+                    .name("흥미 있는 산")
+                    .difficulty(Difficulty.EASY)
+                    .location("충북 청주")
+                    .imageUrl("http://image")
+                    .interest(Interest.ACTIVITY)
+                    .build();
+
+            Mountain mountain2 = Mountain.builder()
+                    .visitCount(10L)
+                    .sequence(0L)
+                    .name("높은 산")
+                    .difficulty(Difficulty.HARD)
+                    .location("충북 청주")
+                    .imageUrl("http://image")
+                    .interest(Interest.HIGH)
+                    .build();
+
+            Mountain mountain3 = Mountain.builder()
+                    .visitCount(11L)
+                    .sequence(0L)
+                    .name("낮은 산")
+                    .difficulty(Difficulty.MODERATE)
+                    .location("충북 청주")
+                    .imageUrl("http://image")
+                    .interest(Interest.LOW)
+                    .build();
+
+            //when
+            mountainRepository.saveMountain(mountain1);
+            mountainRepository.saveMountain(mountain2);
+            mountainRepository.saveMountain(mountain3);
+
+            //then
+            assertThat(mountainRepository.findAllCount()).isEqualTo(mountainCount);
+
+        }
     }
 }
