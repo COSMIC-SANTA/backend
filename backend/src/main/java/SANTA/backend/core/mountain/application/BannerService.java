185ddc0ce3b2f3e8e32614ca53db85026a8a3424
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 10:03:10 +0900

    feat: 배너 표시용 서비스 레이어

--- backend/src/main/java/SANTA/backend/core/mountain/application/BannerService.java
+++ backend/src/main/java/SANTA/backend/core/mountain/application/BannerService.java
@@ -5,5 +5,5 @@ import SANTA.backend.core.user.domain.Interest;
 import org.springframework.http.ResponseEntity;
 
 public interface BannerService {
-    ResponseEntity<BannerResponse> getInterestingMountains(Interest interest);
+    BannerResponse getInterestingMountains(Interest interest);
 }
