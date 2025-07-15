4b6af0ac5370b1c85504ab613e9995206d0f8cdf
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 17:35:28 +0900

    feat: 한국 관강공사 TourAPI 요청 시 사용할 메서드 구현

--- backend/src/main/java/SANTA/backend/global/utils/api/KoreanTourInfoServiceResponder.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/KoreanTourInfoServiceResponder.java
@@ -1,23 +1,102 @@
 package SANTA.backend.global.utils.api;
 
-import SANTA.backend.global.config.RabbitMQConfig;
+import SANTA.backend.global.common.AppProperties;
+import SANTA.backend.global.utils.api.domain.MobileOS;
+import SANTA.backend.global.utils.api.domain.Operation;
+import SANTA.backend.global.utils.api.domain.TouristApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 @RequiredArgsConstructor
-public class KoreanTourInfoServiceResponder implements APIRequestConsumer{
+public class KoreanTourInfoServiceResponder implements APIRequester {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final AppProperties appProperties;
+    private final WebClientService webClientService;
+
+
+    private TouristApiResponse getContent(Operation operation){
+        String url = appProperties.getKoreaTourOrganization().getKoreaTourInfoServiceURL();
+        String key = appProperties.getKoreaTourOrganization().getKoreaTourInfoServiceDecodingKey();
+        String mobileOs = MobileOS.WEB.name();
+        String mobileAppName = appProperties.getKoreaTourOrganization().getMobileAppName();
+
+        return webClientService.request(url,operation.name(), key, mobileOs, mobileAppName);
+    }
 
     @Override
-    @RabbitListener(queues = RabbitMQConfig.KOREAN_TOUR_INFO_SERVICE_QUEUE)
-    public void consumeRequest(String url) {
-        ResponseEntity<Object> response = restTemplate.getForEntity(url, Object.class);
-
+    public TouristApiResponse getContentByAreaCode2() {
+        return getContent(Operation.areaCode2);
     }
+
+    @Override
+    public TouristApiResponse getContentByCategoryCode2() {
+        return getContent(Operation.categoryCode2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByAreaBasedList2() {
+        return getContent(Operation.areaBasedList2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByLocationBasedList2(){
+        return getContent(Operation.locationBasedList2);
+    }
+
+    @Override
+    public TouristApiResponse getContentBySearchKeyword2(){
+        return getContent(Operation.searchKeyword2);
+    }
+
+    @Override
+    public TouristApiResponse getContentBySearchFestival2(){
+        return getContent(Operation.searchFestival2);
+    }
+
+    @Override
+    public TouristApiResponse getContentBySearchStay2(){
+        return getContent(Operation.searchStay2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByDetailCommon2(){
+        return getContent(Operation.detailCommon2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByDetailIntro2(){
+        return getContent(Operation.detailIntro2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByDetailInfo2(){
+        return getContent(Operation.detailInfo2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByDetailImage2(){
+        return getContent(Operation.detailImage2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByAreaBasedSyncList2(){
+        return getContent(Operation.areaBasedSyncList2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByDetailPetTour2(){
+        return getContent(Operation.detailPetTour2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByLdongCode2(){
+        return getContent(Operation.ldongCode2);
+    }
+
+    @Override
+    public TouristApiResponse getContentByLclsSystemCode2(){
+        return getContent(Operation.lclsSystmCode2);
+    }
+
 }
