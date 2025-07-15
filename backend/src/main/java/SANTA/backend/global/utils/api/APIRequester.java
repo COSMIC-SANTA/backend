a487fa38a8665add1767944257b6a46c6549dcbb
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 17:34:54 +0900

    feat: 한국 관강공사 TourAPI 요청 시 사용할 메서드 명시

--- backend/src/main/java/SANTA/backend/global/utils/api/APIRequester.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/APIRequester.java
@@ -1,2 +1,35 @@
-package SANTA.backend.global.utils.api;public interface APIRequester {
+package SANTA.backend.global.utils.api;
+
+import SANTA.backend.global.utils.api.domain.TouristApiResponse;
+
+public interface APIRequester {
+    TouristApiResponse getContentByAreaCode2();
+
+    TouristApiResponse getContentByCategoryCode2();
+
+    public TouristApiResponse getContentByAreaBasedList2();
+
+    TouristApiResponse getContentByLocationBasedList2();
+
+    public TouristApiResponse getContentBySearchKeyword2();
+
+    TouristApiResponse getContentBySearchFestival2();
+
+    TouristApiResponse getContentBySearchStay2();
+
+    TouristApiResponse getContentByDetailCommon2();
+
+    TouristApiResponse getContentByDetailIntro2();
+
+    TouristApiResponse getContentByDetailInfo2();
+
+    TouristApiResponse getContentByDetailImage2();
+
+    TouristApiResponse getContentByAreaBasedSyncList2();
+
+    TouristApiResponse getContentByDetailPetTour2();
+
+    TouristApiResponse getContentByLdongCode2();
+
+    TouristApiResponse getContentByLclsSystemCode2();
 }
