722eee60f1d4769e18f773e56a5898abe895a628
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-15 10:04:38 +0900

    feat: 배너 표시에 필요한 관심사와 산 리스트 표시용 dto 반환하는 메서드 구현

--- backend/src/main/java/SANTA/backend/core/mountain/application/BannerServiceImpl.java
+++ backend/src/main/java/SANTA/backend/core/mountain/application/BannerServiceImpl.java
@@ -1,10 +1,24 @@
 package SANTA.backend.core.mountain.application;
 
+import SANTA.backend.core.mountain.domain.Mountain;
+import SANTA.backend.core.mountain.domain.MountainRepository;
+import SANTA.backend.core.mountain.dto.BannerResponse;
+import SANTA.backend.core.user.domain.Interest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BannerServiceImpl implements BannerService {
 
+    private final MountainRepository mountainRepository;
+
+    @Override
+    public BannerResponse getInterestingMountains(Interest interest) {
+        List<Mountain> bannerMountains = mountainRepository.findByInterest(interest);
+        return BannerResponse.from(interest,bannerMountains);
+    }
 }
