50e52485042c858643255913b749b51f1fc4c884
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-16 11:20:06 +0900

    feat: WebClient의 uri가 String이 아닌 URI값을 갖도록 변경

--- backend/src/main/java/SANTA/backend/global/utils/api/WebClientService.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/WebClientService.java
@@ -1,45 +1,54 @@
 package SANTA.backend.global.utils.api;
 
 import SANTA.backend.global.utils.api.domain.TouristApiResponse;
-import jakarta.annotation.Nullable;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @Component
-@RequiredArgsConstructor
 public class WebClientService {
 
     private final WebClient webClient;
     private static final String JSON = "json";
 
-    public TouristApiResponse request(String url, String operation, String key, String mobileOs, String mobileApp) {
+    public WebClientService(@Qualifier("clientServiceBean") WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<String> request(String url, String operation, String key, String mobileOs, String mobileApp) {
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl(url)                   // http://apis.data.go.kr/...
+                .pathSegment(operation)             // /areaBasedList2
+                .queryParam("serviceKey", key)
+                .queryParam("MobileOS", mobileOs)
+                .queryParam("MobileApp", mobileApp)
+                .queryParam("_type", JSON)
+                .build(true)
+                .toUri();
+
         return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(url+"/"+operation)
-                        .queryParam("serviceKey", key)
-                        .queryParam("MobileOS", mobileOs)
-                        .queryParam("MobileApp", mobileApp)
-                        .queryParam("_type",JSON)
-                        .build())
-                .retrieve().bodyToMono(TouristApiResponse.class)
-                .block();
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(String.class);
     }
 
     public TouristApiResponse request(String url, String operation, String key, String mobileOs, String mobileApp, int contentTyeId) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .path(url+"/"+operation)
+                        .path(url + "/" + operation)
                         .queryParam("serviceKey", key)
                         .queryParam("MobileOS", mobileOs)
                         .queryParam("MobileApp", mobileApp)
-                        .queryParam("contentTypeId",contentTyeId)
-                        .queryParam("_type",JSON)
+                        .queryParam("contentTypeId", contentTyeId)
+                        .queryParam("_type", JSON)
                         .build())
                 .retrieve().bodyToMono(TouristApiResponse.class)
                 .block();
     }
-
 
 
 }
