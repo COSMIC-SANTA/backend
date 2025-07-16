beb6c27baf0ddc363f34b4c7e7f48895c4fbab37
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-16 17:00:35 +0900

    feat: URI 만드는 로직 extract해서 구현

--- backend/src/main/java/SANTA/backend/global/utils/api/KoreanTourInfoServiceURIGenerator.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/KoreanTourInfoServiceURIGenerator.java
@@ -1,2 +1,70 @@
-package SANTA.backend.global.utils.api;public class KoreanTourInfoServiceURIGenerator {
+package SANTA.backend.global.utils.api;
+
+import SANTA.backend.global.utils.api.domain.AreaCode;
+import SANTA.backend.global.utils.api.domain.Arrange;
+import SANTA.backend.global.utils.api.domain.ContentTypeId;
+import jakarta.annotation.Nullable;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+
+public abstract class KoreanTourInfoServiceURIGenerator {
+
+    private static final String JSON = "json";
+
+    protected URI URIGenerator(String url, String operation, String key, String mobileOs, String mobileApp, @Nullable Long numOfRows, @Nullable Long pageNo,
+                               @Nullable Arrange arrange, @Nullable ContentTypeId contentTypeId, @Nullable AreaCode areaCode, @Nullable Long sigunguCode,
+                               @Nullable Long radius, @Nullable Double mapX, @Nullable Double mapY, @Nullable String keyword,
+                               @Nullable String eventStartDate, @Nullable String eventEndDate, @Nullable Long contentId) {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpUrl(url)
+                .pathSegment(operation)
+                .queryParam("serviceKey", key)
+                .queryParam("MobileOS", mobileOs)
+                .queryParam("MobileApp", mobileApp)
+                .queryParam("_type", JSON);
+
+        if (numOfRows != null)
+            builder.queryParam("numOfRows", numOfRows);
+
+        if (pageNo != null)
+            builder.queryParam("pageNo", pageNo);
+
+        if (arrange != null)
+            builder.queryParam("arrange", arrange.name());
+
+        if (contentTypeId != null)
+            builder.queryParam("contentTypeId", contentTypeId.getContentTypeId());
+
+        if (areaCode != null)
+            builder.queryParam("areaCode", areaCode.getCode());
+
+        if (sigunguCode != null)
+            builder.queryParam("sigunguCode", sigunguCode);
+        if (radius != null)
+            builder.queryParam("radius", radius);
+
+        if (mapX != null)
+            builder.queryParam("mapX", mapX);
+
+        if (mapY != null)
+            builder.queryParam("mapY", mapY);
+
+        if (keyword != null)
+            builder.queryParam("keyword", keyword);
+
+        if (eventStartDate != null)
+            builder.queryParam("eventStartDate", eventStartDate);
+
+        if (eventEndDate != null)
+            builder.queryParam("eventEndDate", eventEndDate);
+
+        if (contentId != null)
+            builder.queryParam("contentId", contentId);
+
+        return builder.build(true).toUri();
+    }
+
 }
