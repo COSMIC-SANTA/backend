b7bdfc49c559d242dc21488a7ae6a04957bc24d4
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-16 11:20:59 +0900

    test: 컨트롤러 테스트를 위해 *.Api.class는 import받고 나머지는 모키토빈으로 설정

--- backend/src/test/java/SANTA/backend/context/ControllerTest.java
+++ backend/src/test/java/SANTA/backend/context/ControllerTest.java
@@ -1,2 +1,27 @@
-package SANTA.backend.context;public class ControllerTest {
+package SANTA.backend.context;
+
+import SANTA.backend.core.mountain.api.BannerApi;
+import SANTA.backend.core.mountain.api.MountainApi;
+import SANTA.backend.core.mountain.application.BannerService;
+import SANTA.backend.core.mountain.application.MountainService;
+import SANTA.backend.global.utils.api.APIRequester;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest({MountainApi.class, BannerApi.class})
+@AutoConfigureMockMvc
+public abstract class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockitoBean
+    protected BannerService bannerService;
+
+    @MockitoBean
+    protected MountainService mountainService;
+
 }
