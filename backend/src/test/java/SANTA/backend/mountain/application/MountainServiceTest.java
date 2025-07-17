1167dd8e077da97eb60bd4f5f1cebd796b6b0c6c
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-17 18:05:53 +0900

    feat: 산 id로 산 주변 관광지 찾는 로직 테스트

--- backend/src/test/java/SANTA/backend/mountain/application/MountainServiceTest.java
+++ backend/src/test/java/SANTA/backend/mountain/application/MountainServiceTest.java
@@ -1,2 +1,38 @@
-package SANTA.backend.mountain.application;public class MountainServiceTest {
+package SANTA.backend.mountain.application;
+
+import SANTA.backend.context.ServiceContext;
+import SANTA.backend.core.mountain.domain.Difficulty;
+import SANTA.backend.core.mountain.domain.Mountain;
+import SANTA.backend.core.mountain.dto.MountainNearByResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class MountainServiceTest extends ServiceContext {
+
+    @Test
+    @Transactional
+    void 산_주변_관광지를_조회할_수_있다() {
+        //given
+        String name = "산인데용";
+        String location = "충청북도 청주";
+
+        Mountain mountain = Mountain.builder()
+                .name(name)
+                .location(location)
+                .difficulty(Difficulty.EXTREME)
+                .build();
+
+        mountainService.saveMountain(mountain);
+        List<Mountain> mountains = mountainService.findByName(name);
+
+        //when
+        MountainNearByResponse mountainNearByResponse = mountainService.searchNearByPlacesById(mountains.get(0).getId());
+        System.out.println(mountainNearByResponse);
+
+        //then
+        Assertions.assertThat(mountainNearByResponse).isNotNull();
+    }
 }
