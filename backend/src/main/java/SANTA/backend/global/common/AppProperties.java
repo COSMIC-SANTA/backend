afc4d5e1343ff922a784770b342a2a716b84af02
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-18 10:12:24 +0900

    feat: AppProperties에서 Forest apiKey랑 url 관리하도록 추가

--- backend/src/main/java/SANTA/backend/global/common/AppProperties.java
+++ backend/src/main/java/SANTA/backend/global/common/AppProperties.java
@@ -7,6 +7,7 @@ public class AppProperties {
 
     private final KoreaTourOrganization koreaTourOrganization = new KoreaTourOrganization();
     private final JWT jwt = new JWT();
+    private final Forest forest = new Forest();
 
     public KoreaTourOrganization getKoreaTourOrganization() {
         return this.koreaTourOrganization;
@@ -16,34 +17,36 @@ public class AppProperties {
         return this.jwt;
     }
 
+    public Forest getForest(){return this.forest;}
+
     public static class KoreaTourOrganization {
-        private String koreaTourInfoServiceEncodingKey;
-        private String koreaTourInfoServiceDecodingKey;
-        private String koreaTourInfoServiceURL;
+        private String encodingKey;
+        private String decodingKey;
+        private String url;
         private String mobileAppName;
 
-        public String getKoreaTourInfoServiceEncodingKey() {
-            return koreaTourInfoServiceEncodingKey;
+        public String getEncodingKey() {
+            return encodingKey;
         }
 
-        public void setKoreaTourInfoServiceEncodingKey(String koreaTourInfoServiceEncodingKey) {
-            this.koreaTourInfoServiceEncodingKey = koreaTourInfoServiceEncodingKey;
+        public void setEncodingKey(String encodingKey) {
+            this.encodingKey = encodingKey;
         }
 
-        public String getKoreaTourInfoServiceDecodingKey() {
-            return koreaTourInfoServiceDecodingKey;
+        public String getDecodingKey() {
+            return decodingKey;
         }
 
-        public void setKoreaTourInfoServiceDecodingKey(String koreaTourInfoServiceDecodingKey) {
-            this.koreaTourInfoServiceDecodingKey = koreaTourInfoServiceDecodingKey;
+        public void setDecodingKey(String decodingKey) {
+            this.decodingKey = decodingKey;
         }
 
-        public void setKoreaTourInfoServiceURL(String koreaTourInfoServiceURL){
-            this.koreaTourInfoServiceURL = koreaTourInfoServiceURL;
+        public void setUrl(String url){
+            this.url = url;
         }
 
-        public String getKoreaTourInfoServiceURL(){
-            return koreaTourInfoServiceURL;
+        public String getUrl(){
+            return url;
         }
 
         public void setMobileAppName(String mobileAppName){
@@ -54,7 +57,7 @@ public class AppProperties {
             return mobileAppName;
         }
 
-        public String getKoreaTourInfoServiceRoutingKey(){return koreaTourInfoServiceEncodingKey;}
+        public String getKoreaTourInfoServiceRoutingKey(){return encodingKey;}
     }
 
     public static class JWT{
@@ -63,5 +66,26 @@ public class AppProperties {
         public String getSecret(){return secret;}
 
         public void setSecret(String secret){this.secret = secret;}
+    }
+
+    public static class Forest{
+        private String url;
+        private String key;
+
+        public void setUrl(String url){
+            this.url = url;
+        }
+
+        public String getUrl(){
+            return this.url;
+        }
+
+        public void setKey(String key){
+            this.key = key;
+        }
+
+        public String getKey(){
+            return this.key;
+        }
     }
 }
