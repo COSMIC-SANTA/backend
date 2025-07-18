be5ebbf6d48984ba39afac1e6a44b7779632f7a4
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-18 09:03:25 +0900

    refactor: pageNo 파라미터로 받도록 변경

--- backend/src/main/java/SANTA/backend/global/utils/api/APIRequester.java
+++ backend/src/main/java/SANTA/backend/global/utils/api/APIRequester.java
@@ -4,5 +4,5 @@ import SANTA.backend.core.mountain.dto.MountainNearByResponse;
 import reactor.core.publisher.Mono;
 
 public interface APIRequester {
-    public Mono<MountainNearByResponse> searchNearByPlacesByLocation(String placeName);
+    public Mono<MountainNearByResponse> searchNearByPlacesByLocation(String placeName, Long pageNo);
 }
