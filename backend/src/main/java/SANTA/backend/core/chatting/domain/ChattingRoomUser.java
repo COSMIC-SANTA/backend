7c8912d85ed17379f79f9e5aac72fc7956dccef4
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-21 13:35:02 +0900

    feat: 채팅 참가자 도메인 구현

--- backend/src/main/java/SANTA/backend/core/chatting/domain/ChattingRoomUser.java
+++ backend/src/main/java/SANTA/backend/core/chatting/domain/ChattingRoomUser.java
@@ -1,2 +1,14 @@
-package SANTA.backend.core.chatting.domain;public class ChattingRoomUser {
+package SANTA.backend.core.chatting.domain;
+
+import SANTA.backend.core.chatting.entity.ChattingRoomEntity;
+import SANTA.backend.core.user.entity.UserEntity;
+import lombok.Getter;
+
+@Getter
+public class ChattingRoomUser {
+    Long id;
+
+    UserEntity user;
+
+    ChattingRoomEntity chatting;
 }
