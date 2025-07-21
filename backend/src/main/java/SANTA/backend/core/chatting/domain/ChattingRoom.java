f8d070b3864ec226a7ab09374f202adc9f2eaa73
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-21 13:34:26 +0900

    feat: 채팅방 도메인 구현

--- backend/src/main/java/SANTA/backend/core/chatting/domain/ChattingRoom.java
+++ backend/src/main/java/SANTA/backend/core/chatting/domain/ChattingRoom.java
@@ -1,10 +1,22 @@
 package SANTA.backend.core.chatting.domain;
 
+import SANTA.backend.core.chatting.entity.ChattingRoomUserEntity;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
-public class Chatting {
+public class ChattingRoom {
 
     private Long id;
+
+    private String title;
+
+    private String subTitle;
+
+    private String clientUrl;
+
+    private List<ChattingRoomUserEntity> chattingRoomUserEntities = new ArrayList<>();
 
 }
