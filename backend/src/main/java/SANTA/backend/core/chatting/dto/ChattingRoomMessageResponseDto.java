e3a0012195851a03ca63305513e140fb7ddfd9ca
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-22 14:08:36 +0900

    feat: 메시지 rest 응답용 dto

--- backend/src/main/java/SANTA/backend/core/chatting/dto/ChattingRoomMessageResponseDto.java
+++ backend/src/main/java/SANTA/backend/core/chatting/dto/ChattingRoomMessageResponseDto.java
@@ -1,2 +1,15 @@
-package SANTA.backend.core.chatting.dto;public class ChattingRoomMessageResponseDto {
+package SANTA.backend.core.chatting.dto;
+
+import SANTA.backend.core.chatting.domain.ChattingRoomMessage;
+
+import java.time.LocalDateTime;
+
+public record ChattingRoomMessageResponseDto(
+        LocalDateTime createdAt,
+        String userName,
+        String message
+) {
+    public static ChattingRoomMessageResponseDto from(ChattingRoomMessage chattingRoomMessage){
+        return new ChattingRoomMessageResponseDto(chattingRoomMessage.getCreatedAt(),chattingRoomMessage.getChattingRoomUser().getUser().getNickname(), chattingRoomMessage.getMessage());
+    }
 }
