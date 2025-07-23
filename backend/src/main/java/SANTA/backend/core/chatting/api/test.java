c6977749cc1b39d48687340513a5d835a5829796
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-23 11:13:20 +0900

    test: 채팅 전송 태스트

--- backend/src/main/java/SANTA/backend/core/chatting/api/test.java
+++ backend/src/main/java/SANTA/backend/core/chatting/api/test.java
@@ -1,2 +1,13 @@
-package SANTA.backend.core.chatting.api;public class test {
+package SANTA.backend.core.chatting.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+
+    @GetMapping("/chattingTest")
+    public String chattingTest(){
+        return "chattingTest";
+    }
 }
