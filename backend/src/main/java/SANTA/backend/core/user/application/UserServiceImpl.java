d68d1e7e7ce8f7af1e31a40b3d784c20a4114ca3
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-30 10:30:26 +0900

    refactor: id로 불러온 사용자 정보를 캐시에 저장하고 이후 조회부터 캐시에서 불러오도록 변경

--- backend/src/main/java/SANTA/backend/core/user/application/UserServiceImpl.java
+++ backend/src/main/java/SANTA/backend/core/user/application/UserServiceImpl.java
@@ -4,6 +4,7 @@ import SANTA.backend.core.user.domain.User;
 import SANTA.backend.core.user.domain.UserRepository;
 import SANTA.backend.core.user.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
+    @Cacheable(cacheNames = "users", key = "'userId:'+ #userId", cacheManager = "cacheManager")
     public User findById(Long userId){
         return userRepository.findById(userId);
     }
