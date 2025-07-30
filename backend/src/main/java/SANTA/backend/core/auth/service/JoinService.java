3eada7fe1efa974798187a18742f0fd57268899d
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-30 11:39:43 +0900

    feat: 캐시에서 userId에해당하는 value 삭제 후 토큰을 validate 시간동안 블랙리스트로 redis에 저장

--- backend/src/main/java/SANTA/backend/core/auth/service/JoinService.java
+++ backend/src/main/java/SANTA/backend/core/auth/service/JoinService.java
@@ -3,18 +3,28 @@ package SANTA.backend.core.auth.service;
 import SANTA.backend.core.auth.dto.JoinResponseDTO;
 import SANTA.backend.core.user.domain.User;
 import SANTA.backend.core.user.domain.UserRepository;
+import SANTA.backend.global.jwt.JWTUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class JoinService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder; //config에 있는 encode방식
+    private final JWTUtil jwtUtil;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JWTUtil jwtUtil, RedisTemplate<String,String> redisTemplate) {
         this.userRepository = userRepository; //초기화 과정
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.redisTemplate = redisTemplate;
     }
 
     public User findByUsername(String username) {
@@ -34,5 +44,13 @@ public class JoinService {
 
         User joinUser = userRepository.join(user);
         return new JoinResponseDTO(joinUser);//repository에서 db로의 데이터 저장??
+    }
+
+    @CacheEvict(cacheNames = "users", key = "'userId:'+#userId",cacheManager = "cacheManager")
+    public void logOut(Long userId, String accessToken) {
+        if (accessToken == null || jwtUtil.isExpired(accessToken))
+            throw new IllegalArgumentException("유효하지 않은 토큰");
+        long expiration = jwtUtil.getExpiration(accessToken) - System.currentTimeMillis();
+        redisTemplate.opsForValue().set("blacklist:"+accessToken,"logout", expiration, TimeUnit.MILLISECONDS);
     }
 }
