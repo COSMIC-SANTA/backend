241ba96bed600ba82fb7e86dbb931c25e4762d62
sinsohi <sohi8451@naver.com>
2025-08-08 04:03:08 +0900

    test: 산 이동 최적 경로 조회 테스트

--- backend/src/test/java/SANTA/backend/context/ControllerTest.java
+++ backend/src/test/java/SANTA/backend/context/ControllerTest.java
@@ -4,10 +4,13 @@ import SANTA.backend.core.mountain.api.BannerApi;
 import SANTA.backend.core.mountain.api.MountainApi;
 import SANTA.backend.core.mountain.application.BannerService;
 import SANTA.backend.core.mountain.application.MountainService;
-import SANTA.backend.global.utils.api.APIRequester;
+import SANTA.backend.core.user.application.UserService;
+import SANTA.backend.global.jwt.JWTUtil;
+import SANTA.backend.global.utils.api.rabbitmq.RabbitMQRequester;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,5 +26,18 @@ public abstract class ControllerTest {
 
     @MockitoBean
     protected MountainService mountainService;
+
+    @MockitoBean
+    protected RabbitMQRequester rabbitMQRequester;
+
+    @MockitoBean
+    protected JWTUtil jwtUtil;
+
+    @MockitoBean
+    protected UserService userService;
+
+    @MockitoBean
+    protected RedisTemplate<String, String> redisTemplate;
+
 
 }
