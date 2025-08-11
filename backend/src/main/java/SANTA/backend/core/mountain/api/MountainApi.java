befb918c53d3232ea12320bef78724597e4d415b
sinsohi <sohi8451@naver.com>
2025-08-11 22:45:43 +0900

    feat: 주변 편의시설 조회를 위한 api 작성

--- backend/src/main/java/SANTA/backend/core/mountain/api/MountainApi.java
+++ backend/src/main/java/SANTA/backend/core/mountain/api/MountainApi.java
@@ -1,6 +1,8 @@
 package SANTA.backend.core.mountain.api;
 
 import SANTA.backend.core.mountain.application.MountainService;
+import SANTA.backend.core.mountain.dto.MountainFacilityRequest;
+import SANTA.backend.core.mountain.dto.MountainFacilityResponse;
 import SANTA.backend.core.mountain.dto.MountainNearByResponse;
 import SANTA.backend.core.mountain.dto.MountainSearchResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +26,11 @@ public class MountainApi {
     public ResponseEntity<MountainNearByResponse> mountainNearBy(@PathVariable("mountainId") Long mountainId, @PathVariable("pageNo") Long pageNo){
         MountainNearByResponse mountainNearByResponse = mountainService.searchNearByPlacesById(mountainId,pageNo);
         return ResponseEntity.ok().body(mountainNearByResponse);
+    }
+
+    @PostMapping("/facilities")
+    public ResponseEntity<MountainFacilityResponse> searchFacilities(@RequestBody MountainFacilityRequest mountainFacilityRequest){
+        MountainFacilityResponse mountainFacilityResponse = mountainService.searchFacilities(mountainFacilityRequest);
+        return ResponseEntity.ok().body(mountainFacilityResponse);
     }
 }
