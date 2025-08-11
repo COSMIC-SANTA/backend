b7258615a33e324bbc310fdc84133c682e2f216f
sinsohi <sohi8451@naver.com>
2025-08-11 21:46:10 +0900

    refactor: Interest 파라미터를 String으로 변경하고 수동 변환 처리

--- backend/src/main/java/SANTA/backend/core/banner/api/BannerApi.java
+++ backend/src/main/java/SANTA/backend/core/banner/api/BannerApi.java
@@ -1,6 +1,5 @@
 package SANTA.backend.core.banner.api;
 
-import SANTA.backend.core.auth.service.CustomUserDetails;
 import SANTA.backend.core.banner.application.BannerService;
 import SANTA.backend.core.banner.dto.BannerResponse;
 import SANTA.backend.core.user.domain.Interest;
@@ -8,7 +7,6 @@ import SANTA.backend.global.common.ResponseHandler;
 import SANTA.backend.global.utils.api.rabbitmq.RabbitMQRequester;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,15 +24,15 @@ public class BannerApi {
     }
 
     @GetMapping("/banner")
-    public ResponseEntity<ResponseHandler<BannerResponse>> getBanner(Interest interest, @RequestParam(defaultValue = "best") String type){
+    public ResponseEntity<ResponseHandler<BannerResponse>> getBanner(@RequestParam(required = false) String interest, @RequestParam(defaultValue = "best") String type){
 
         BannerResponse response;
         if ("interest".equals(type)){
-            response = bannerService.getInterestingMountains(interest);
+            Interest userInterest = Interest.valueOf(interest);
+            response = bannerService.getInterestingMountains(userInterest);
         } else {
             response = bannerService.getPopularMountains();
         }
         return ResponseEntity.ok().body(ResponseHandler.success(response));
     }
-
 }
