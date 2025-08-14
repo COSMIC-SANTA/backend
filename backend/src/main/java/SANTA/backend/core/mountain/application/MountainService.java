6aa7920799c9abdb6e4b2afa54a6653cf22e61b9
sinsohi <sohi8451@naver.com>
2025-08-14 14:43:14 +0900

    refactor: 필요없는 부분 삭제

--- backend/src/main/java/SANTA/backend/core/mountain/application/MountainService.java
+++ backend/src/main/java/SANTA/backend/core/mountain/application/MountainService.java
@@ -1,6 +1,5 @@
 package SANTA.backend.core.mountain.application;
 
-import SANTA.backend.core.banner.application.BannerService;
 import SANTA.backend.core.mountain.domain.Mountain;
 import SANTA.backend.core.mountain.domain.MountainRepository;
 import SANTA.backend.core.mountain.dto.MountainFacilityRequest;
@@ -9,38 +8,23 @@ import SANTA.backend.core.mountain.dto.MountainNearByResponse;
 import SANTA.backend.core.mountain.dto.MountainSearchResponse;
 import SANTA.backend.core.mountain.dto.OptimalRouteRequest;
 import SANTA.backend.core.mountain.dto.OptimalRouteResponse;
-import SANTA.backend.core.mountain.entity.MountainEntity;
-import SANTA.backend.global.exception.ErrorCode;
-import SANTA.backend.global.exception.type.CustomException;
 import SANTA.backend.global.utils.api.APIRequester;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
 public class MountainService {
 
-    private final WebClient webClient;
-
-    @Value("${forest.api.key}")
-    private String forestApiKey;
-
     private final MountainRepository mountainRepository;
-    private final BannerService bannerService;
     private final APIRequester apiRequester;
 
-    public MountainService(@Qualifier("forestApiClient") WebClient webClient, MountainRepository mountainRepository, APIRequester apiRequester, BannerService bannerService) {
-        this.webClient = webClient;
+    public MountainService(MountainRepository mountainRepository, APIRequester apiRequester) {
         this.mountainRepository = mountainRepository;
-        this.bannerService = bannerService;
         this.apiRequester = apiRequester;
     }
 
