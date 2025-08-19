3615e7f1fbc149b826da48699e9d721ade09d4ef
정선웅 <tjsdnd1219@naver.com>
2025-08-19 16:28:52 +0900

    Update SuccessHandler.java

--- backend/src/main/java/SANTA/backend/global/oauth/handler/SuccessHandler.java
+++ backend/src/main/java/SANTA/backend/global/oauth/handler/SuccessHandler.java
@@ -1,7 +1,12 @@
 package SANTA.backend.global.oauth.handler;
 
+import SANTA.backend.core.auth.util.CookieUtil;
+import SANTA.backend.global.common.AppProperties;
+import SANTA.backend.global.exception.ErrorCode;
+import SANTA.backend.global.exception.type.CustomException;
 import SANTA.backend.global.jwt.JWTUtil;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +17,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+import static SANTA.backend.global.oauth.handler.HttpCookieOAuth2Auth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Slf4j
 @Component
@@ -20,24 +29,46 @@ public class SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JWTUtil jwtUtil;
 
+    private final HttpCookieOAuth2Auth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+    private final AppProperties appProperties;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
 
-        String targetUrl=determineTargetUrl(request,response,authentication);
+        String targetUrl = determineTargetUrl(request, response, authentication);
         log.info("🔁 리다이렉트 대상 URL = {}", targetUrl);
-        clearAuthenticationAttributes(request);
-        getRedirectStrategy().sendRedirect(request,response,targetUrl);
+        clearAuthenticationAttributes(request,response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,Authentication authentication) {
-        String redirectUri = "http://localhost:8080/abc";
-        //String redirectUri=CookieUtil.getCookie(request,REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue).get();
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        Optional<String> redirectUri = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue);
 
-        String token=jwtUtil.createJwt(authentication);
+        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED_REDIRECT_URI);
+        }
 
-        return UriComponentsBuilder.fromUriString(redirectUri)
-                .queryParam("token",token)
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+
+        String token = jwtUtil.createJwt(authentication);
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", token)
                 .build().toUriString();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        URI clientRedirectUri = URI.create(uri);
+        String authorizedRedirectUris = appProperties.getOAuth2().getAuthorizedRedirectUris();
+        URI authorizedURI = URI.create(authorizedRedirectUris);
+        return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost()) && authorizedURI.getPort() == clientRedirectUri.getPort();
     }
 }
